82b5813c9a831d67965b5183e0ea5746b0aa1e7a
anticrisis-assistance <75688847+anticrisis-assistance@users.noreply.github.com>
2020-12-09 10:44:26 +0300

    fill data

--- NewToken.sol
+++ NewToken.sol
@@ -4,11 +4,16 @@ import "./ERC20Standard.sol";
 
 contract NewToken is ERC20Standard {
 	constructor() public {
-		totalSupply = 123;
-		name = "Best ever coin";
-		decimals = 4;
-		symbol = "BEC";
+		totalSupply = 300000000000000;
+		name = "natural social human token";
+		decimals = 2;
+		symbol = "NSH";
 		version = "1.0";
+		nominalPriceInUSD = 1;
+		theIssuer = "CHARITY FUND 'ANTI-CRISIS ASSISTANCE' OGRN 1175476048241";
+		ensuringTheValueOfTokens = "natural, social, human capitals of Russia";
+		goalsOfImplementingOfTokens = "providing rights to users of open-fund.site for growth of personal and total natural, social, human capitals in Russia";
+		
 		balances[msg.sender] = totalSupply;
 	}
-}
+}	
